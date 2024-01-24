455b16ccc857ac29d5c57e298217f5f03fe34fd9
up220007 <up220007@alumnos.upa.edu.mx>
2024-01-24 09:02:55 -0600

    last code 1 update
    adding one clear screen function and works in windows and linux

--- Unidad_1/Ejercicio_1.cpp
+++ Unidad_1/Ejercicio_1.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <cstdlib>
+#endif
+void limpiarPantalla();
+void salir();
 double calculo(double renta, double impuessto);
 bool verifier(int lecturas,double renta);
 bool salida = false;
 double renta = 0;
 int lecturas;
-void salir();
 int main(int argc, char const *argv[])
 {
     printf("Bienvenido al sistema calculador de impuestos\n");
@@ -20,12 +26,14 @@ int main(int argc, char const *argv[])
         {
             double impuesto = calculo(renta,5.0);
             double total = impuesto + renta;
+            limpiarPantalla();
             printf("El impuesto basandonos en su renta de $%0.02lf que le corresponde es del 05%% por lo tanto su impuesto es $%0.02lf y su total es de %0.02lf \n",renta,impuesto,total);
         }
         else if ((renta >= 10000) && (renta <= 20000))
         {
             double impuesto = calculo(renta,15.0);
             double total = impuesto + renta;
+            limpiarPantalla();
             printf("El impuesto basandonos en su renta de $%0.02lf que le corresponde es del 15%% por lo tanto su impuesto es $%0.02lf y su total es de %0.02lf \n",renta,impuesto,total);
         }
         else if ((renta > 20000) && (renta <= 35000))
@@ -38,12 +46,14 @@ int main(int argc, char const *argv[])
         {
             double impuesto = calculo(renta,30.0);
             double total = impuesto + renta;
+            limpiarPantalla();
             printf("El impuesto basandonos en su renta de $%0.02lf que le corresponde es del 30%% por lo tanto su impuesto es $%0.02lf y su total es de %0.02lf \n",renta,impuesto,total);
         }
         else if (renta > 60000)
         {
             double impuesto = calculo(renta,45.0);
             double total = impuesto + renta;
+            limpiarPantalla();
             printf("El impuesto basandonos en su renta de $%0.02lf que le corresponde es del 45%% por lo tanto su impuesto es $%0.02lf y su total es de %0.02lf \n",renta,impuesto,total);
         }
         printf("deseas seguir calculando impuestos? (s = si / n=no): ");
@@ -59,10 +69,13 @@ double calculo(double renta, double impuessto){
 
 bool verifier(int lecturas, double renta){
     if (lecturas != 1) {
-        while (getchar() != '\n');
+        while (getchar() != '\n'){
+            limpiarPantalla();
             printf("Error: Por favor, ingrese un número válido y no un caracter.\n");
+        }
         return false;
     } else if (renta < 0) {
+        limpiarPantalla();
         printf("Error: Por favor, ingrese un número positivo válido.\n");
         return false;
     } else {
@@ -78,13 +91,29 @@ void salir(){
         if (out == 's' || out == 'S')
         {
             salida = false;
+            limpiarPantalla();
             printf("Sigamos calculando entonces\n");
         }
         else if (out == 'n' || out == 'N')
         {
             salida =  true;
             renta = 0;
+            limpiarPantalla();
             printf("nos vemos pasa un ecelente dia\n");
+        }else if(out != 'n'&& out != 's' && out != 'N' && out != 'S' && out != ' ' && out != '\n')
+        {
+            limpiarPantalla();
+            printf("Caracter no valido\n");
+            printf("deseas seguir calculando impuestos? (s = si / n=no): ");
         }
+        
     }while (out != 'n'&& out != 's' && out != 'N' && out != 'S');
+}
+
+void limpiarPantalla() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 }
